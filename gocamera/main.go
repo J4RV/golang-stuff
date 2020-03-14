ed10551e8b7fad65f79428ed8fa13d738befc92b
Jarv <kira-8@hotmail.com>
2020-03-15 00:19:17 +0100

    Added test flag

--- gocamera/main.go
+++ gocamera/main.go
@@ -4,29 +4,42 @@ import (
 	"fmt"
 	"os/exec"
 	"time"
+	"flag"
 
 	"github.com/robfig/cron/v3"
 )
 
-const EVERY_FOUR_HOURS = "0/4 ? * * *"
+const EVERY_FOUR_HOURS = "0 0/4 ? * * *"
+const EVERY_30_SECS    = "0/30 * * * * *"
+
+var testFlag bool
 
 func main() {
-	c := cron.New()
+    flag.BoolVar(&testFlag, "test", false, "Takes a picture every 30 seconds instead")
+    flag.Parse()
 
-	_, err := c.AddFunc(EVERY_FOUR_HOURS, takePicture)
+	c := cron.New(cron.WithSeconds())
+
+    cronExp := EVERY_FOUR_HOURS
+    if testFlag {
+        cronExp = EVERY_30_SECS
+    }
+
+	_, err := c.AddFunc(cronExp, takePicture)
 	if err != nil {
 		panic(err)
 	}
 
 	c.Start()
-	fmt.Println("CRON started successfully")
+	fmt.Println("CRON started successfully with expression: " + cronExp)
 	select {} // permanent sleep, don't exit main
 }
 
 func takePicture() {
 	name := time.Now().Format("2006.01.02 15:04:05")
-	c := exec.Command("raspistill", "-o", "./"+name)
+	path := "$HOME/photos/"+name
+	c := exec.Command("raspistill", "-o", path)
 	if c.Run() != nil {
-		fmt.Println("Couldn't take picture", name)
+		fmt.Println("Couldn't take picture", path)
 	}
 }
